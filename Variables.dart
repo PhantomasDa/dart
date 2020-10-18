a377c8ae2ddead1b4b90d8fbf29b64c3b89a4689
Christian rios <mockraw@gmail.com>
2020-10-18 18:23:57 -0500

    cambio de nombre

---
+++ Variables.dart
@@ -0,0 +1,20 @@
+main(List<String> args) {
+int valInt = 1;
+double valDoble = 1.0;
+String valString = 'hola';
+bool valBool = true;;
+
+
+print('$valString mi nombre es argel');
+
+print(valInt);
+print(valDoble);
+print(valString);
+print(valBool);
+ls
+
+
+print(valString.isEmpty
+
+
+}
---
+++ estosiescolleciones.dart
@@ -0,0 +1,8 @@
+main(List<String> args) {
+  List lista;
+  lista = ['negro', 'rojo'];
+
+  lista.add('rosa');
+  lista.add('rosas');
+  print(lista);
+}
